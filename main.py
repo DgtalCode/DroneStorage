c5765da54fc85d60fcce9faa2bdc8e3d8eabdd30
Dgtal1 <helloworld.danil@gmail.com>
2022-01-24 20:48:23 +0300

    Добавлен расчет линейных коррекций для выравнивая по маркеру в центре изображения

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import cv2
 import numpy as np
+from pioneer_sdk import Pioneer
 
 ###################### ОБЪЯВЛЕНИЕ ПЕРЕМЕННЫХ ######################
-cap = cv2.VideoCapture(0)
+# ыбор источника видео:
+# 0 - камера компьютера
+# 1 - камера квадрокоптера
+flag_video_source = 0
+
+if flag_video_source == 1:
+    pioneer = Pioneer(logger=False, bad_connection_exit=False)
+    if pioneer.bad_connection_occured():
+        flag_video_source = 0
+
+if flag_video_source == 0:
+    cap = cv2.VideoCapture(0)
+
 
 # параметры детектируемых маркеров
 arDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
 arPars = cv2.aruco.DetectorParameters_create()
 
 # параметры ПД регулятора для выравниания по маркерам
-k1 = 1
-k2 = 1
+k1 = 0.0008
+k2 = 0.002
 err = 0
 errold = 0
 u = 0
@@ -31,7 +44,27 @@ def get_aruco_center(corners):
     '''
     x = np.sum([ c[0] for c in corners[0] ]) // 4
     y = np.sum([ c[1] for c in corners[0] ]) // 4
-    return np.int(x), np.int(y)
+    return int(x), int(y)
+
+
+def vec_from_points(point_start, point_end):
+    return (point_end[0] - point_start[0],
+            point_start[1] - point_end[1])
+
+
+def vec_length(vec):
+    return np.sqrt( vec[0]**2 + vec[1]**2 )
+
+
+def vec_direction(vec, to_degrees=False):
+    angle = np.arctan2(vec[1], vec[0])
+    if to_degrees:
+        angle = np.degrees(angle)
+    return angle
+
+
+def distance_between_points(point1, point2):
+    return np.sqrt( (point2[0] - point1[0])**2 + (point2[1] - point1[1])**2 )
 ###################################################################
 
 
@@ -41,18 +74,31 @@ if len(frame) != H:
     flag_resize = True
 
 while True:
+    # считывание изображения
     _, frame = cap.read()
     if flag_resize:
         frame = cv2.resize(frame, (640, 480))
 
     (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, arDict, parameters=arPars)
-
     if corners:
         frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids, (255,0,0))
 
-    for c in corners:
-        x, y = get_aruco_center(c)
+    if len(corners) == 1:
+        x, y = get_aruco_center(corners[0])
         cv2.drawMarker(frame, (x,y), (0,0,255), cv2.MARKER_CROSS)
+
+        error_vec = vec_from_points((W // 2, H // 2), (x, y))
+        error_vec_dir = vec_direction(error_vec)
+        cv2.arrowedLine(frame, (W // 2, H // 2), (x, y), (255, 0, 255), 2)
+
+        err = vec_length(error_vec)
+        u = k1*err - k2*(err-errold)
+        errold = err
+
+        x_correction = round(u * np.cos(error_vec_dir), 4)
+        y_correction = round(u * np.sin(error_vec_dir), 4)
+
+        print(x_correction, y_correction)
 
     cv2.imshow('frame', frame)
 
