8d99964137bca8b9cf4adc22f60caa3c70cc3c5d
Dgtal1 <helloworld.danil@gmail.com>
2022-02-07 15:06:20 +0300

    Рабочий пролет

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
 import random
-
+import sys
 import cv2
 import numpy as np
-from pioneer_sdk import Pioneer
+from pioneer_sdk.asynchronous import Pioneer
 from pioneer_sdk import pioutils
-import pygame
 
 ###################### ОБЪЯВЛЕНИЕ ПЕРЕМЕННЫХ ######################
-# выбор источника видео:
-# 0 - камера компьютера
-# 1 - камера квадрокоптера
-flag_video_source = 1
 
-flag_rc_control = False
-
-if flag_video_source == 1:
-    pioneer = Pioneer(bad_connection_exit=False, heartbeat_logger=True)
-    if pioneer.bad_connection_occured():
-        flag_video_source = 0
-
-if flag_video_source == 0:
-    cap = cv2.VideoCapture(0)
+# объект для взаимодействия с квадрокоптером
+pioneer = Pioneer(logger=False)
 
 # параметры детектируемых маркеров
 arDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_1000)  # тип маркера
 arPars = cv2.aruco.DetectorParameters_create()  # параметры детектирования (стандартные)
 
+# массив, хранящий найденные маркеры по позициям
+database = []
+
 # позиционировнаие коптера
-posx, posy, posz, yaw = 0, 0, 1.5, 0  # координаты коптера (м)
-mvxy = 0.5  # шаг перемещений по Х У (м)
-mvz = 0.2  # шаг перемещений по Z (м)
-mvyaw = np.radians(10)  # шаг поворота (град)
+mvxy = -1  # шаг перемещений по Х У (м)
+mvz = 1.7  # шаг перемещений по Z (м)
+mvyaw = np.radians(90)  # шаг поворота (град)
 
 # параметры ПД регулятора для выравниания по маркерам
 k1 = 0.12  # реакция на отклонение
@@ -43,197 +33,151 @@ u = 0  # управляющее воздействие
 
 # ширина и высота изображения
 W, H = (640, 480)
-# флаг необходимости изменять размер изображения под установленный выше
-flag_resize = False
-
-control_w = 602
-control_h = 300
-left_stick_pos = ((control_w - 2) // 4, control_h // 2)
-right_stick_pos = ((control_w - 2) // 4, control_h // 2)
-
-pygame.init()
-screen = pygame.display.set_mode((control_w, control_h))
-pygame.display.set_caption('CONTROLS')
-clock = pygame.time.Clock()
-
 
 ###################################################################
 
 
 ######################## ОБЪЯВЛЕНИЕ ФУНКЦИЙ #######################
-def get_aruco_center(corners):
+def get_aruco_center(corns):
     """
     Считает координаты центра маркера по переданнм четырем углам
-    :param corners: массив с коодинатами углов маркера
+    :param corns: массив с коодинатами углов маркера
     :return: массив с координатами центра маркера
     """
-    x = np.sum([c[0] for c in corners[0]]) // 4
-    y = np.sum([c[1] for c in corners[0]]) // 4
+    x = np.sum([c[0] for c in corns]) // 4
+    y = np.sum([c[1] for c in corns]) // 4
     return int(x), int(y)
-
-
-def get_frame():
-    """
-    Возвращает opencv фрейм от квадрокоптера или камеры ПК, в зависимоти от используемого источника
-    :return: фрейм
-    """
-    global flag_video_source
-    global frame
-    if flag_video_source == 0:
-        _, frame = cap.read()
-    else:
-        frame = cv2.imdecode(np.frombuffer(pioneer.get_raw_video_frame(), dtype=np.uint8), cv2.IMREAD_COLOR)
-    return frame
 
 
 def nothing(x):
     pass
 
 
+def add_marker(name, data_raw):
+    """
+    Добавляет информацию о считанном маркере в базу данных (массив)
+    :return:
+    """
+    global database
+    data = None
+    if data_raw is not None:
+        data_raw = data_raw[1][0]
+        print(data_raw)
+        data = {
+            name: {
+                'type': data_raw & 0b111110000,
+                'subtype': data_raw & 0b000001111
+            }
+        }
+    else:
+        data = {
+            name: 'empty'
+        }
+    database.append(data)
 ###################################################################
 
 
 ############################### MAIN ##############################
-frame = get_frame()
-if len(frame) != H:
-    flag_resize = True
-
-STEP = 0
-
+rows = 1
+cols = 2
+row_cur = 1
+col_cur = 1
 while True:
-    screen.fill((255, 255, 255))
-    pygame.draw.line(screen, (0, 0, 0), (control_w // 2, 0), (control_w // 2, control_h), 2)
-
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEMOTION:
-            if event.buttons == (1, 0, 0):
-                if event.pos[0] < control_w // 2:
-                    left_stick_pos = event.pos
-                    right_stick_pos = ((control_w - 2) // 4, control_h // 2)
-                else:
-                    right_stick_pos = (event.pos[0] - (control_w - 2) // 2, event.pos[1])
-                    left_stick_pos = ((control_w - 2) // 4, control_h // 2)
-            else:
-                left_stick_pos = ((control_w - 2) // 4, control_h // 2)
-                right_stick_pos = ((control_w - 2) // 4, control_h // 2)
-        if event.type == pygame.WINDOWLEAVE:
-            left_stick_pos = ((control_w - 2) // 4, control_h // 2)
-            right_stick_pos = ((control_w - 2) // 4, control_h // 2)
-
-    pygame.draw.circle(screen, (100, 100, 100), left_stick_pos, 20, 20)
-    pygame.draw.circle(screen, (100, 100, 100), (right_stick_pos[0] + (control_w - 2) // 2, right_stick_pos[1]), 20, 20)
-    pygame.display.flip()
-
     # считывание изображения
-    frame = get_frame()
-    if flag_resize:
-        frame = cv2.resize(frame, (640, 480))
+    frame = cv2.imdecode(np.frombuffer(pioneer.get_raw_video_frame(), dtype=np.uint8), cv2.IMREAD_COLOR)
 
-    print(pioneer.step_get(), pioneer.get_task_id())
+    # детектирование маркеров
+    corners, ids, rejected = cv2.aruco.detectMarkers(frame, arDict, parameters=arPars)
+
+    if corners:
+        markers_data = list(enumerate(corners))
+        markers_data = sorted(markers_data, key=lambda md: pioutils.distance_between_points(
+            get_aruco_center(md[1][0]),
+            (W//2, H//2)
+        ), reverse=True)
+        markers_data = (markers_data[0][1], ids[markers_data[0][0]])
+    else:
+        markers_data = None
+
+    # отображение маркеров на изображении
+    if corners:
+        frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids, (255, 0, 0))
 
     if pioneer.step_get() == 0:
         pioneer.arm()
         pioneer.takeoff()
-        if pioneer.in_air():    pioneer.step_inc()
+        if pioneer.in_air():
+            pioneer.step_inc()
 
     if pioneer.step_get() == 1:
-        pioneer.go_to_local_point(0.5, 0, 1.5, callback=pioneer.step_inc)
+        pioneer.go_to_local_point(x=0, y=0, z=mvz * row_cur, callback=lambda: pioneer.step_inc(2))
 
     if pioneer.step_get() == 2:
-        pioneer.sleep(1, callback=pioneer.step_inc)
+        u, err, error_vec_dir = 0, 0, 0
+
+        if corners:
+            # расчет центра маркера
+            x, y = get_aruco_center(corners[0])
+            cv2.drawMarker(frame, (x, y), (0, 0, 255), cv2.MARKER_CROSS)
+
+            # расчет отклонения маркера от центра изображения в виде вектора
+            error_vec = pioutils.vec_from_points((W // 2, H // 2), (x, y))
+            error_vec_dir = round(pioutils.vec_direction(error_vec, to_degrees=True))
+            cv2.arrowedLine(frame, (W // 2, H // 2), (x, y), (255, 0, 255), 2)
+
+            # расчет упавляющего воздействия через ПД регулятор
+            # для удержания маркера в центре изображения
+            err = pioutils.vec_length(error_vec)
+            errsum += err
+            u = k1 * err + k2 * (err - errold) + k3 * errsum
+            u = round(min(u, 100))
+            errold = err
+
+            print(err, u)
+
+            pioneer.vector_speed_control((u, error_vec_dir), (0, 0), min_val=0, max_val=100,
+                                         use_polar=True, use_zy_xr_vectors=True, degrees=True)
+
+            if err < 25:
+                pioneer.send_rc_channels(1500, 1500, 1500, 1500, 1500, 2000)
+                pioneer.step_inc()
+        else:
+            pioneer.vector_speed_control((0, 0), (0, 0), min_val=0, max_val=100,
+                                         use_polar=True, use_zy_xr_vectors=True, degrees=True)
 
     if pioneer.step_get() == 3:
-        pioneer.go_to_local_point(-0.5, 1, 1, callback=pioneer.step_inc)
+        pioneer.go_to_local_point(x=0, y=mvxy * col_cur, z=mvz * rows, callback=pioneer.step_inc)
 
     if pioneer.step_get() == 4:
-        pioneer.sleep(1, pioneer.step_inc)
-
-    # if pioneer.step_get() == 2:
-    #     u, err, error_vec_dir = 0, 0, 0
-    #
-    #     # детектирование маркеров
-    #     (corners, ids, rejected) = cv2.aruco.detectMarkers(frame, arDict, parameters=arPars)
-    #     # отображение маркеров на изображении
-    #     if corners:
-    #         frame = cv2.aruco.drawDetectedMarkers(frame, corners, ids, (255, 0, 0))
-    #
-    #     if len(corners) == 1:
-    #         # расчет центра маркера
-    #         x, y = get_aruco_center(corners[0])
-    #         cv2.drawMarker(frame, (x, y), (0, 0, 255), cv2.MARKER_CROSS)
-    #
-    #         # расчет отклонения маркера от центра изображения в виде вектора
-    #         error_vec = pioutils.vec_from_points((W // 2, H // 2), (x, y))
-    #         error_vec_dir = round(pioutils.vec_direction(error_vec, to_degrees=True))
-    #         cv2.arrowedLine(frame, (W // 2, H // 2), (x, y), (255, 0, 255), 2)
-    #
-    #         # расчет упавляющего воздействия через ПД регулятор
-    #         # для удержания маркера в центре изображения
-    #         err = pioutils.vec_length(error_vec)
-    #         errsum += err
-    #         u = k1 * err + k2 * (err - errold) + k3 * errsum
-    #         u = round(min(u, 100))
-    #         errold = err
-    #
-    #         print(err, u)
-    #
-    #         pioneer.vector_speed_control((u, error_vec_dir), (0, 0), min_val=0, max_val=100,
-    #                                      use_polar=True, use_zy_xr_vectors=True, degrees=True)
-    #
-    #         if err < 25:
-    #             pioneer.send_rc_channels(1500, 1500, 1500, 1500, 1500, 2000)
-    #             pioneer.step_inc()
-    #     else:
-    #         pioneer.vector_speed_control((0, 0), (0, 0), min_val=0, max_val=100,
-    #                                      use_polar=True, use_zy_xr_vectors=True, degrees=True)
+        pioneer.sleep(1, callback=(lambda: add_marker(col_cur + (row_cur-1)*rows, markers_data), pioneer.step_inc))
 
     if pioneer.step_get() == 5:
+        if col_cur == cols:
+            col_cur = 0
+            if row_cur != rows:
+                row_cur += 1
+                pioneer.step_reset(1)
+            else:
+                pioneer.step_inc()
+        else:
+            col_cur += 1
+            pioneer.step_reset(3)
+
+    if pioneer.step_get() == 6:
+        pioneer.go_to_local_point(x=0, y=0, z=1, callback=pioneer.step_inc)
+
+    if pioneer.step_get() == 7:
         pioneer.land()
+        if pioneer.landed():
+            break
 
     cv2.imshow('frame', frame)
     key = cv2.waitKey(1)
-    if key == ord('k'):
-        print("k")
+    if key == ord('q'):
         break
 
-    if flag_video_source == 1:
-        if key == 32:
-            print('space pressed')
-            pioneer.arm()
-        if key == ord('t'):
-            print('t pressed')
-            pioneer.takeoff()
-        if key == ord('l'):
-            print('l pressed')
-            pioneer.land()
-        if key == 27:  # esc
-            print('esc pressed')
-            pioneer.disarm()
-        if key == ord('g'):
-            print('g pressed')
-            flag_rc_control = not flag_rc_control
-            print(f'Flag now is {flag_rc_control}')
-        if key == ord('r'):
-            print('r pressed')
-            pioneer.go_to_local_point(x=round(random.uniform(-1.5, 1.5), 2),
-                                      y=round(random.uniform(-1.5, 1.5), 2),
-                                      z=round(random.uniform(1.0, 1.5), 2))
-
-
-        # проверка режима полета: по скоростям или по точкам
-        # if flag_rc_control:
-        #     # если пользователь не перехватывает управление, то позиционируемся по маркерам
-        #     # в противном случае слушаемся пользователя
-        #     if sum(left_stick_pos) + sum(right_stick_pos) != 600:
-        #         pioneer.vector_speed_control(left_stick_pos, right_stick_pos, min_val=0, max_val=300,
-        #                                      rev_left_x=True, rev_right_x=True)
-        #     else:
-        #         pioneer.vector_speed_control((u, error_vec_dir), (0, 0), min_val=0, max_val=100,
-        #                                      use_polar=True, use_zy_xr_vectors=True, degrees=True)
-        # else:
-        #     # важнейший параметр - 6й канал в 2000, так как возвращает коптер в режим полета по точкам
-        #     pioneer.send_rc_channels(channel_1=1500, channel_2=1500, channel_3=1500,
-        #                              channel_4=1500, channel_5=1500, channel_6=2000)
-
 cv2.destroyAllWindows()
-cap.release()
+
+database = np.array(database)
+database.shape = (rows, cols)
+print(database)
